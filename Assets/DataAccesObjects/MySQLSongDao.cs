a2187e7219c57d22bdc8d1253a99fb3dc52833a3
Snake-Pl1ssken <pelukijl@gmail.com>
2025-06-05 13:25:40 +0200

    ahora ya se borran las canciones de la base de datos solo quedarioa hacer que se recuperen y se reproduzcan+

--- Assets/DataAccesObjects/MySQLSongDao.cs
+++ Assets/DataAccesObjects/MySQLSongDao.cs
@@ -16,7 +16,7 @@ public class MySQLSongDao : SongStatsDao
     public string database = "drumhero";
     public string user = "root";
     public string password = "";
-    public int port = 3306;
+    public int port = 3307;
     private MySqlConnection connection;
     Song song;
 
@@ -51,18 +51,50 @@ public class MySQLSongDao : SongStatsDao
 
     public void DeleteSong(string id)
     {
-        throw new System.NotImplementedException();
-        //Borrar una cancion de la tabla Song
+        string query = "DELETE FROM songs WHERE id = '" + id + "'";
+        MySqlCommand command = new MySqlCommand(query, connection);
+
+        command.ExecuteNonQuery();
     }
 
     public List<SongEvent> GetSongEvents()
     {
-        throw new System.NotImplementedException();
+        List<SongEvent> events = new List<SongEvent>();
+
+        string query = "SELECT data, time FROM SongEvents"; 
+        MySqlCommand command = new MySqlCommand(query, connection);
+
+        MySqlDataReader reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            SongEvent songEvent = new SongEvent
+            {
+                data = reader.GetFloat("data"),
+                time = reader.GetFloat("time")
+            };
+
+            events.Add(songEvent);
+        }
+
+        return events;
     }
 
     public List<string> GetSongIds()
     {
-        throw new System.NotImplementedException();
+        List<string> ids = new List<string>();
+
+        string query = "SELECT id FROM Songs";
+        MySqlCommand command = new MySqlCommand(query, connection);
+        MySqlDataReader reader = command.ExecuteReader();
+        
+        while (reader.Read())
+        {
+            string id = reader.GetString("id");
+            ids.Add(id);
+        }
+        
+        return ids;
     }
 
     public void GetSongInfo(out string _title, out string _author)
@@ -78,19 +110,17 @@ public class MySQLSongDao : SongStatsDao
 
         string query = "SELECT title, author FROM Songs WHERE id = '" + id + "'";
         MySqlCommand command = new MySqlCommand(query, connection);
-
-        using (MySqlDataReader reader = command.ExecuteReader())
+        MySqlDataReader reader = command.ExecuteReader();
+        
+        if (reader.Read())
         {
-            if (reader.Read())
-            {
-                _title = reader.GetString("title");
-                _author = reader.GetString("author");
-            }
-            else
-            {
-                Debug.Log("No esta cancion con id: " + id);
-            }
+            _title = reader.GetString("title");
+            _author = reader.GetString("author");
         }
+        else
+        {
+            Debug.Log("No esta cancion con id: " + id);
+        }        
     }
 
     public void LoadSong(string id)
@@ -129,7 +159,7 @@ public class MySQLSongDao : SongStatsDao
 
 
                 command = new MySqlCommand(insertEventQuery, connection);
-                command.ExecuteNonQuery();
+                command.ExecuteNonQuery(); //ExecuteNonQuery para no devolver resultado
             }
         }
         catch (Exception ex)
@@ -145,94 +175,3 @@ public class MySQLSongDao : SongStatsDao
         song.author = _author;
     }
 }
-
-
-
-//public class MySQLRaceStatsDAO : RaceStatsDAO
-//{
-//    // Datos de conexión
-//    [SerializeField] string server = "127.0.0.1";
-//    [SerializeField] string database = "racer";
-//    [SerializeField] string user = "root";
-//    [SerializeField] string password = "";
-//    [SerializeField] int port = 3307;
-//    private MySqlConnection connection;
-
-//    public MySQLRaceStatsDAO()
-//    {
-//        string connectionString = "Server=127.0.0.1; Port=3307; Database=racer; Uid=root; Pwd=;";
-//        Debug.Log(connectionString);
-//        connection = new MySqlConnection(connectionString);
-
-//        connection.Open();
-//        Debug.Log("Connected 2 MySQL");
-//    }
-
-//    public RaceStats FindRaceStats(string id)
-//    {
-//        RaceStats raceStats = new RaceStats();
-//        raceStats.gates = new List<GateStat>();
-
-//        string query = "SELECT start_time FROM race_stats WHERE id = @id";
-//        MySqlCommand cmd = new MySqlCommand(query, connection);
-//        cmd.Parameters.AddWithValue("@id", id);
-
-//        using (MySqlDataReader reader = cmd.ExecuteReader())
-//        {
-//            if (reader.Read())
-//            {
-//                raceStats.startTime = reader.GetDateTime("start_time");
-//            }
-//            else
-//            {
-//                Debug.LogWarning("No se encontró la carrera en MySQL.");
-//                return raceStats;
-//            }
-//        }
-
-//        query = "SELECT time, speed FROM gates WHERE raceId = @id ORDER BY gateIndex";
-//        cmd = new MySqlCommand(query, connection);
-//        cmd.Parameters.AddWithValue("@id", id);
-
-//        using (MySqlDataReader reader = cmd.ExecuteReader())
-//        {
-//            while (reader.Read())
-//            {
-//                GateStat stat = new GateStat
-//                {
-//                    time = reader.GetFloat("time"),
-//                    speed = reader.GetFloat("speed")
-//                };
-
-//                raceStats.gates.Add(stat);
-//            }
-//        }
-
-//        return raceStats;
-//    }
-
-//    public string SaveRaceStats(RaceStats s)
-//    {
-//        string raceId = Guid.NewGuid().ToString();
-
-//        string query = "INSERT INTO race_stats (id, start_time) VALUES (@id, @start_time)";
-//        MySqlCommand cmd = new MySqlCommand(query, connection);
-//        cmd.Parameters.AddWithValue("@id", raceId);
-//        cmd.Parameters.AddWithValue("@start_time", s.startTime);
-//        cmd.ExecuteNonQuery();
-//        Debug.Log("llego aqui");
-//        for (int i = 0; i < s.gates.Count; i++)
-//        {
-//            GateStat gate = s.gates[i];
-//            query = "INSERT INTO gates (raceId, gateIndex, time, speed) VALUES (@raceId, @gateIndex, @time, @speed)";
-//            cmd = new MySqlCommand(query, connection);
-//            cmd.Parameters.AddWithValue("@raceId", raceId);
-//            cmd.Parameters.AddWithValue("@gateIndex", i);
-//            cmd.Parameters.AddWithValue("@time", gate.time);
-//            cmd.Parameters.AddWithValue("@speed", gate.speed);
-//            cmd.ExecuteNonQuery();
-//        }
-//        Debug.Log("llego aqui2");
-//        return raceId;
-//    }
-//}
